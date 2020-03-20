e1ed3dd103c732b3f603e3f693b10a962a078d37
kdgosik <kdgosik@gmail.com>
2020-03-20 17:03:10 -0400

    updated labs and rendered bookdown site

--- docker/install.R
+++ docker/install.R
@@ -1,27 +1,27 @@
 ## CRAN Packages
 install.packages('umap')
 # install.packages("ggplot2")
-# install.packages("gam")
+install.packages("gam")
 # install.packages("ggbeeswarm")
 # install.packages("ggthemes")
 # install.packages("dplyr")
 # install.packages("plyr")
 # install.packages("Matrix")
 # install.packages("fossil")
-#install.packages("BiocManager")
+install.packages("BiocManager")
 # install.packages("devtools")
 # install.packages('rmarkdown')
 #install.packages("rgl", dependencies=TRUE)
 
 ## Bioconductor Packages
 #BiocManager::install("SingleCellExperiment", version = "3.8", update = FALSE)
-#BiocManager::install("destiny", version = "3.8", update = FALSE)
-#BiocManager::install("slingshot", version = "3.8", update = FALSE)
-#BiocManager::install("splatter", version = "3.8", update = FALSE)
-#BiocManager::install("scater", version = "3.8", update = FALSE)
-#BiocManager::install("DropletUtils", version = "3.8", update = FALSE)
-#BiocManager::install("pcaMethods", version = "3.8", update = FALSE)
-#BiocManager::install("CountClust", version = "3.8", update = FALSE)
+#BiocManager::install("destiny", update = FALSE)
+#BiocManager::install("slingshot", update = FALSE)
+#BiocManager::install("splatter", update = FALSE)
+#BiocManager::install("scater", update = FALSE)
+#BiocManager::install("DropletUtils", update = FALSE)
+#BiocManager::install("pcaMethods", update = FALSE)
+BiocManager::install("CountClust", update = FALSE)
 
 
 ## Github Packages
