9f451bc3f2f956ce112da1ba7de8da0bce1bccd6
kdgosik <kdgosik@gmail.com>
2020-03-23 16:43:28 -0400

    updated docker scripts

--- docker/run_docker_aws.sh
+++ docker/run_docker_aws.sh
@@ -3,7 +3,7 @@
 PORT=$1
 
 # -v $PWD:/home/rstudio \
-sudo docker run --rm -ti \
+docker run --rm -ti \
 -v $PWD:/home/rstudio \
 -e PASSWORD=train \
 -p $PORT:8787 kdgosik/2020scworkshop
--- docker/run_docker_bash.sh
+++ docker/run_docker_bash.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # -v $PWD:/home/rstudio \
-sudo docker run --rm -ti \
+docker run --rm -ti \
 -v $PWD:/home/rstudio kdgosik/2020scworkshop bash
